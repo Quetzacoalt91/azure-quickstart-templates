3cdd446b39d37ca86c29086a624b742246f917ef
Quetzacoalt91 <thomas.nabord@prestashop.com>
2015-09-21 17:11:50 +0200

    Implement auto install

--- prestashop/install-prestashop.sh
+++ prestashop/install-prestashop.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+fqdn=$1
+db_server=$2
+db_password=$3
+user_firstname=$4
+user_lastname=$5
+user_email=$6
+user_password=$7
+
+
 apt-get -y update
 
 apt-get -y install wget unzip
@@ -40,4 +49,13 @@ chown -R www-data:www-data $root_path;
 chmod -R g+w $root_path;
 
 
-logger "PrestaShop is ready to be installed !"
+logger "Installing PrestaShop, this may take a while ..."
+
+php $root_path/install-dev/index_cli.php --domain=$fqdn --db_server=$db_server --db_name="prestashop" --db_user=root \
+			--db_password=$db_password --firstname="$user_firstname" --lastname="$user_lastname" \
+			--password=$user_password --email="$user_email" \
+			--newsletter=0 --send_email=0
+
+chown www-data:www-data -R /var/www/html/
+
+logger "PrestaShop in installed !"
