3a5d2f7707279fdae129c93c0ee6253cf5143daa
Quetzacoalt <thomas.nabord@prestashop.com>
2016-01-28 15:17:01 +0100

    Add expire on static files

--- prestashop/install-prestashop.sh
+++ prestashop/install-prestashop.sh
@@ -25,7 +25,7 @@ else
 	apt-get -y install apache2 unzip
 	apt-get -y install php5 php5-fpm php5-cli php5-mysql php5-mcrypt php5-curl php5-gd
 	a2dismod mpm_prefork mpm_event
-	a2enmod rewrite actions alias proxy proxy_fcgi mpm_worker
+	a2enmod rewrite actions alias proxy proxy_fcgi mpm_worker expires
 
 	mv ./php.ini /etc/php5/fpm/php.ini
 	mv ./000-default.conf /etc/apache2/sites-available/000-default.conf
